4d378f9ccea0d2e8b193541d8868aad2d2053b18
desertcoqui <fdetres@my365.bellevue.edu>
2023-02-10 10:29:02 -0600

    Create USER API

--- server/routes/user-api.js
+++ server/routes/user-api.js
@@ -6,18 +6,83 @@
  */
 
 // require statements
-const express = require('express');
-const User = require('../models/user');
-const bcrypt = require('bcryptjs');
-const BaseResponse = require('../services/base-response');
-const ErrorResponse = require('../services/error-response');
+const express = require("express");
+const User = require("../models/user");
+const bcrypt = require("bcryptjs");
+const BaseResponse = require("../services/base-response");
+const ErrorResponse = require("../services/error-response");
 
 const router = express.Router();
 const saltRounds = 10; // default salt rounds for hashing algorithm
+/**
+ * createUser
+ * @openapi
+ * /api/users:
+ *   post:
+ *     tags:
+ *       - Users
+ *     description: API to create new user objects
+ *     summary: Creates a new user object
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             required:
+ *               - userName
+ *               - password
+ *             properties:
+ *              userName:
+ *                type: string
+ *              password:
+ *                type: string
+ *     responses:
+ *       '200':
+ *         description: Query successful
+ *       '500':
+ *         description: Internal server error
+ *       '501':
+ *         description: MongoDB Exception
+ */
+router.post("/", async (req, res) => {
+  try {
+    let hashedPassword = bcrypt.hashSync(req.body.password, saltRounds); // salt/hash the password
+    standardRole = {
+      text: "standard",
+    };
 
+    // user object
+    let newUser = {
+      userName: req.body.userName,
+      password: hashedPassword,
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      phoneNumber: req.body.phoneNumber,
+      address: req.body.address,
+      email: req.body.email,
+      role: standardRole,
+    };
+
+    User.create(newUser, function (err, user) {
+      if (err) {
+        console.log(err);
+        const createUserMongodbErrorResponse = new ErrorResponse(500, "Internal server error", err);
+        res.status(500).send(createUserMongodbErrorResponse.toObject());
+      } else {
+        console.log(user);
+        const createUserResponse = new BaseResponse(200, "Query successful", user);
+        res.json(createUserResponse.toObject());
+      }
+    });
+  } catch (e) {
+    console.log(e);
+    const createUserCatchErrorResponse = new ErrorResponse(500, "Internal server error", e.message);
+    res.status(500).send(createUserCatchErrorResponse.toObject());
+  }
+});
 
 /**
- * openapi: 3.0.0
+ * findAll
  * @openapi
  * /api/users:
  *  get:
@@ -25,47 +90,36 @@ const saltRounds = 10; // default salt rounds for hashing algorithm
  *          - Users
  *      description: This Api for returning an array users objects
  *      summary: Return a list of all users
- *      responses:          
+ *      responses:
  *          '200':
  *              description: array of users
  *          '500':
  *              description: Server Exception.
  *          '501':
- *              description: MongoDB Exception              
+ *              description: MongoDB Exception
  */
-/**
- * FindAll
- */
-
-router.get('/', async (req, res)=>{
-    try 
-    {
-        User.find({}, function(err, users){
-            if(err){
-                console.log(err);
-                const findAllMongodbErrorResponse = new ErrorResponse(500, 'Internal server error', err);
-                res.status(500).send(findAllMongodbErrorResponse.toObject());
-
-            }
-            else{
-                console.log(users);
-                const findAllUsersResponse = new BaseResponse(200, 'Query successful', users)
-                res.json(findAllUsersResponse.toObjet());
-            }
-        })
-
-    }catch (e){
-        const findAllCatchErrorResponse = new ErrorResponse(500, 'Internal server error', e.message);
-        res.status(500).send(findAllCatchErrorResponse.toObject());
-
-    }
-})
-
-
+router.get("/", async (req, res) => {
+  try {
+    User.find({}, function (err, users) {
+      if (err) {
+        console.log(err);
+        const findAllMongodbErrorResponse = new ErrorResponse(500, "Internal server error", err);
+        res.status(500).send(findAllMongodbErrorResponse.toObject());
+      } else {
+        console.log(users);
+        const findAllUsersResponse = new BaseResponse(200, "Query successful", users);
+        res.json(findAllUsersResponse.toObjet());
+      }
+    });
+  } catch (e) {
+    const findAllCatchErrorResponse = new ErrorResponse(500, "Internal server error", e.message);
+    res.status(500).send(findAllCatchErrorResponse.toObject());
+  }
+});
 /**
  * updateUser
  * @openapi
- * /api/user/:id
+ * /api/user/{id}
  *  post:
  *      tags:
  *          - user
@@ -84,52 +138,42 @@ router.get('/', async (req, res)=>{
  *          '500':
  *              description: Server Exception
  *          '501':
- *              description: MongoDB Exception 
+ *              description: MongoDB Exception
  */
+router.post("/:id", async (req, res) => {
+  try {
+    User.findOne({ _id: req.params.id }, function (err, user) {
+      if (err) {
+        console.log(err);
+        const updateUserMongodbErrorResponse = new ErrorResponse(500, "Internal server error", err);
+        res.status(500).send(updateUserMongodbErrorResponse.toObject());
+      } else {
+        console.log(user);
 
-
-/**
- * updateUser
- */
-
-router.post('/:id', async(req, res)=>{
-    try 
-    {
-        User.findOne({'_id': req.params.id}, function(err, user){
-            if(err){
-                console.log(err);
-                const updateUserMongodbErrorResponse = new ErrorResponse(500, 'Internal server error', err);
-                res.status(500).send(updateUserMongodbErrorResponse.toObject());
-            }else{
-                console.log(user);
-
-                user.set({
-                    firstName: req.body.firstName,
-                    lastName: req.body.lastName,
-                    phoneNumber: req.body.phoneNumber,
-                    address: req.body.address,
-                    email: req.body.email
-                })
-                user.save(function(err, savedUser){
-                    if(err){
-                        console.log(err);
-                        const saveUserMongodbErrorResponse = new ErrorResponse(500, 'Iternal server error', err)
-                        res.status(500).send(saveUserMongodbErrorResponse.toObject());
-                    }else {
-                        console.log(savedUser);
-                        const saveUserResponse = new BaseResponse(200, 'Query successful', savedUser);
-                        res.json(saveUserResponse.toObject());
-                    }
-                }) 
-            }
-        })
-
-    }
-    catch (e){        
-        console.log(e);
-        const updateUserCatchErrorResponse = new ErrorResponse(500, 'Internal server error', e.message);
-        res.status(500).send(updateUserCatchErrorResponse.toObject())
-
-    }
-})
+        user.set({
+          firstName: req.body.firstName,
+          lastName: req.body.lastName,
+          phoneNumber: req.body.phoneNumber,
+          address: req.body.address,
+          email: req.body.email,
+        });
+        user.save(function (err, savedUser) {
+          if (err) {
+            console.log(err);
+            const saveUserMongodbErrorResponse = new ErrorResponse(500, "Iternal server error", err);
+            res.status(500).send(saveUserMongodbErrorResponse.toObject());
+          } else {
+            console.log(savedUser);
+            const saveUserResponse = new BaseResponse(200, "Query successful", savedUser);
+            res.json(saveUserResponse.toObject());
+          }
+        });
+      }
+    });
+  } catch (e) {
+    console.log(e);
+    const updateUserCatchErrorResponse = new ErrorResponse(500, "Internal server error", e.message);
+    res.status(500).send(updateUserCatchErrorResponse.toObject());
+  }
+});
 module.exports = router;
