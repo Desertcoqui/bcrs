1795503733025f0c07c23ca9d710789c28994ca4
desertcoqui <fdetres@my365.bellevue.edu>
2023-02-13 11:11:49 -0600

    package json

--- src/app/shared/base-layout/base-layout.component.ts
+++ src/app/shared/base-layout/base-layout.component.ts
@@ -1,16 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+/* // Title: Bobs Computer Repair Shop
+// Author: Professor Krasso
+// Date: Feb 10 2023
+// Modified By: Ferdinand Detres Jr
+// Attributions: https://www.section.io/engineering-education/nodejs-mongoosejs-mongodb/
+//https://www.youtube.com/watch?v=WDrU305J1yw
+// In-Class tutorials */
+
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-base-layout',
-  templateUrl: './base-layout.component.html',
-  styleUrls: ['./base-layout.component.css']
+  selector: "app-base-layout",
+  templateUrl: "./base-layout.component.html",
+  styleUrls: ["./base-layout.component.css"],
 })
 export class BaseLayoutComponent implements OnInit {
   year: number = Date.now();
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
