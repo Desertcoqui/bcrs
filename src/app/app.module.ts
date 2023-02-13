e4ffcb69f6869b9c8d43bcf463224ebe74cfb141
desertcoqui <fdetres@my365.bellevue.edu>
2023-02-13 08:30:38 -0600

    module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,66 +7,36 @@ import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { HomeComponent } from "./pages/home/home.component";
 import { AuthLayoutComponent } from "./shared/auth-layout/auth-layout.component";
 import { BaseLayoutComponent } from "./shared/base-layout/base-layout.component";
+import { HttpClient, HttpClientModule } from "@angular/common/http";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+
 import { FlexLayoutModule } from "@angular/flex-layout";
 import { MatToolbarModule } from "@angular/material/toolbar";
 import { MatButtonModule } from "@angular/material/button";
 import { MatIconModule } from "@angular/material/icon";
-import { SecurityQuestionDetailsComponent } from "./pages/security-question-details/security-question-details.component";
-import { UserCreateComponent } from "./pages/user-create/user-create.component";
-import { LoginComponent } from "./pages/login/login.component";
-import { AppComponent } from "./app.component";
-import { AppRoutingModule } from "./app-routing.module";
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { FormsModule, ReactiveFormsModule } from "@angular/forms";
-import { HttpClient, HttpClientModule } from "@angular/common/http";
-
-/**
- * Components
- */
-// Pages
-import { HomeComponent } from "./pages/home/home.component";
-import { SecurityQuestionDetailsComponent } from "./pages/security-question-details/security-question-details.component";
-import { SecurityQuestionListComponent } from "./pages/security-question-list/security-question-list.component";
-import { LoginComponent } from "./pages/login/login.component";
-import { UserCreateComponent } from "./pages/user-create/user-create.component";
-import { UserDetailsComponent } from "./pages/user-details/user-details.component";
-import { UserListComponent } from "./pages/user-list/user-list.component";
-
-// Shared
-import { AuthLayoutComponent } from "./shared/auth-layout/auth-layout.component";
-import { BaseLayoutComponent } from "./shared/base-layout/base-layout.component";
-
-// Services
-import { CookieService } from "ngx-cookie-service";
-
-/**
- * Angular material imports
- */
-import { FlexLayoutModule } from "@angular/flex-layout";
-import { MatButtonModule } from "@angular/material/button";
+import { MatMenuModule } from "@angular/material/menu";
 import { MatCardModule } from "@angular/material/card";
 import { MatCheckboxModule } from "@angular/material/checkbox";
 import { MatDialogModule } from "@angular/material/dialog";
 import { MatDividerModule } from "@angular/material/divider";
 import { MatFormFieldModule } from "@angular/material/form-field";
-import { MatIconModule } from "@angular/material/icon";
 import { MatInputModule } from "@angular/material/input";
 import { MatListModule } from "@angular/material/list";
-import { MatMenuModule } from "@angular/material/menu";
 import { MatNativeDateModule, MatRippleModule } from "@angular/material/core";
 import { MatPaginatorModule } from "@angular/material/paginator";
 import { MatSelectModule } from "@angular/material/select";
 import { MatSortModule } from "@angular/material/sort";
 import { MatStepperModule } from "@angular/material/stepper";
 import { MatTableModule } from "@angular/material/table";
-import { MatToolbarModule } from "@angular/material/toolbar";
-
-// Dialog
+import { SecurityQuestionDetailsComponent } from "./pages/security-question-details/security-question-details.component";
+import { UserCreateComponent } from "./pages/user-create/user-create.component";
+import { LoginComponent } from "./pages/login/login.component";
+import { SecurityQuestionListComponent } from "./pages/security-question-list/security-question-list.component";
+import { UserDetailsComponent } from "./pages/user-details/user-details.component";
+import { UserListComponent } from "./pages/user-list/user-list.component";
+import { CookieService } from "ngx-cookie-service";
 import { DialogModule } from "@angular/cdk/dialog";
-
-/**
- * PrimeNG
- */
+// PrimeNG
 import { ButtonModule } from "primeng/button";
 import { ConfirmationService, ConfirmEventType } from "primeng/api";
 import { ConfirmDialogModule } from "primeng/confirmdialog";
@@ -75,16 +45,16 @@ import { TableModule } from "primeng/table";
 @NgModule({
   declarations: [
     AppComponent,
+    HomeComponent,
     AuthLayoutComponent,
     BaseLayoutComponent,
-    DeleteRecordDialogComponent,
-    HomeComponent,
-    SecurityQuestionDetailsComponent,
-    SecurityQuestionListComponent,
     LoginComponent,
     UserCreateComponent,
     UserDetailsComponent,
     UserListComponent,
+    DeleteRecordDialogComponent,
+    SecurityQuestionDetailsComponent,
+    SecurityQuestionListComponent,
   ],
   imports: [
     AppRoutingModule,
@@ -115,10 +85,11 @@ import { TableModule } from "primeng/table";
     MatTableModule,
     MatToolbarModule,
     MessageModule,
+    MessagesModule,
     ReactiveFormsModule,
     TableModule,
   ],
-  providers: [CookieService],
+  providers: [],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
