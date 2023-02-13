86bc523153884a4aa0f77af41ecc7280e58040c8
desertcoqui <fdetres@my365.bellevue.edu>
2023-02-13 10:25:02 -0600

    module routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -13,6 +13,8 @@ import { HomeComponent } from "./pages/home/home.component";
 import { SecurityQuestionDetailsComponent } from "./pages/security-question-details/security-question-details.component";
 import { AuthLayoutComponent } from "./shared/auth-layout/auth-layout.component";
 import { SecurityQuestionsListComponent } from "./pages/security-questions-list/security-questions-list.component";
+import { UserCreateComponent } from "./pages/user-create/user-create.component";
+import { UserDetailsComponent } from "./pages/user-details/user-details.component";
 import { UserListComponent } from "./pages/user-list/user-list.component";
 import { AuthGuard } from "./auth.guard";
 import { LoginComponent } from "./pages/login/login.component";
@@ -32,11 +34,15 @@ const routes: Routes = [
       },
       {
         path: "users/:userId",
-        component: HomeComponent,
+        component: UserDetailsComponent,
       },
       {
         path: "users/create/new",
         component: UserDetailsComponent,
+      },
+      {
+        path: "users/create/new",
+        component: UserCreateComponent,
       },
       {
         path: "security-questions",
