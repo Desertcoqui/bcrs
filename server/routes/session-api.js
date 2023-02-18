a999e92ad78da0ea0f2a50f492d33f572d4e2151
Kailee Stephens <kaistephens1@my365.bellevue.edu>
2023-02-18 12:51:54 -0600

    register/password-reset API's

--- server/routes/session-api.js
+++ server/routes/session-api.js
@@ -110,7 +110,6 @@ router.post("/login", async (req, res) => {
     res.status(500).send(signinCatchErrorResponse.toObject());
   }
 });
-
 /**
  * VerifyUser
  * @openapi
@@ -166,6 +165,155 @@ router.get('/verify/users/:userName', async (req, res)=>{
     res.status(500).send(verifyUserCatchErrorResponse.toObject());
 
   }
-})
+});
+/**
+ * register
+ * @openapi
+ * /api/session/register:
+ *   post:
+ *     tags:
+ *       - Session
+ *     description: API to register user
+ *     summary: Register user
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             required:
+ *               - userName
+ *               - password
+ *             properties:
+ *              userName:
+ *                type: string
+ *              password:
+ *                type: string
+ *     responses:
+ *       '200':
+ *         description: Login successful
+ *       '401':
+ *         description: Invalid username and/or password, please try again
+ *       '500':
+ *         description: Internal server error
+ *       '501':
+ *         description: MongoDB Exception
+ */
+router.post('/register', async(req, res) => {
+  try {
+    User.findOne({'userName': req.body.userName}, function(err, user)
+    {
+      if(err) {
+        console.log(err);
+        const registerUserMongodbErrorResponse = new ErrorResponse('500', 'Internal server error', err);
+        res.status(500).send(registerUserMongodbErrorResponse.toObject());
+      } else {
+        if (!user) {
+          let hashedPassword = bycrypt.hashSync(req.body.password, saltRounds); // salt/hash password
+          standardRole = {
+            text: 'standard'
+          }
+
+          //user object 
+          let registeredUser = {
+            userName: req.body.userName,
+            password: hashedPassword,
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
+            phoneNumber: req.body.phoneNumber,
+            address: req.body.address,
+            email: req.body.email,
+            role: standardRole,
+            selectedSecurityQuestions: req.body.selectedSecurityQuestions
+          };
+
+          User.create(registeredUser, function(err, newUser) {
+            if (err) {
+              console.log(err);
+              const newUserMongodbErrorResponse = new ErrorResponse('500', 'Internal server error', err);
+            } else {
+              console.log(newUser); 
+              const registeredUserResponse = new BaseResponse('200', 'Query successful', newUser);
+              res.json(registeredUserResponse.toObject());
+            }
+          })
+        } else {
+          console.log('Username ${req.body.userName} already exists.');
+          const userInUseError = new BaseResponse('400', 'The username ${req.body.userName} is already in use.', null);
+          res.status(400).send(userInUseError.toObject());
+        }
+      }
+    })
+  } catch (e) {
+    console.log(e);
+    const registeredUserCatchErrorResponse = new ErrorResponse('500', 'Internal server error', e.message);
+    res.status(500).send(registeredUserCatchErrorResponse.toObject());
+  }
+});
+/**
+ * resetPassword
+ * @openapi
+ * /api/session/users/:username/reset-password:
+ *   post:
+ *     tags:
+ *       - Session
+ *     description: API to reset password
+ *     summary: Reset a users password
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             required:
+ *               - userName
+ *               - password
+ *             properties:
+ *              userName:
+ *                type: string
+ *              password:
+ *                type: string
+ *     responses:
+ *       '200':
+ *         description: Login successful
+ *       '401':
+ *         description: Invalid username and/or password, please try again
+ *       '500':
+ *         description: Internal server error
+ *       '501':
+ *         description: MongoDB Exception
+ */
+router.post('/users/:username/reset-password', async(req, res) => {
+  try {
+    const password = req.body.password;
+    User.findOne({'username': req.params.userName}, function(err, user) {
+      if (err) {
+        console.log(err);
+        const resetPasswordMongodbErrorResponse = new ErrorResponse('500', 'Internal server error', err);
+        res.status(500).send(resetPasswordMongodbErrorResponse.toObject());
+      } else {
+        console.log(user);
+        let hashedPassword = bycrypt.hashSync(password, saltRounds); //salt/hash password
+        user.set({
+          password: hashedPassword
+        });
+
+        user.save(function(err, updatedUser) {
+          if (err) {
+            console.log(err);
+            const updatedUserMongodbErrorREsponse = new ErrorResponse('500', 'Internal server error', err);
+            res.status(500).send(updatedUserMongodbErrorREsponse.toObject());
+          } else {
+            console.log(updatedUser);
+            const updatedPasswordResponse = new BaseResponse('200', 'Query successful', updatedUser);
+            res.json(updatedPasswordResponse.toObject());
+          }
+        })
+      }
+    })
+  } catch (e) {
+    console.log(e);
+    const resetPasswordCatchError = new ErrorResponse('500', 'Internal server error', e);
+    res.status(500).send(resetPasswordCatchError.toObject());
+  }
+});
 
 module.exports = router;
