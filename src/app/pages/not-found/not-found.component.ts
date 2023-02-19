2c6407f319ea952d839b117f94a9d4cf620cf2c7
desertcoqui <fdetres@my365.bellevue.edu>
2023-02-19 09:12:57 -0600

    not-found

---
+++ src/app/pages/not-found/not-found.component.ts
@@ -0,0 +1,20 @@
+// <!-- // Title: Bobs Computer Repair Shop
+// // Author: Professor Krasso
+// // Date: Feb 10 2023
+// // Modified By: Ferdinand Detres Jr
+// // Attributions: https://www.section.io/engineering-education/nodejs-mongoosejs-mongodb/
+// //https://www.youtube.com/watch?v=WDrU305J1yw
+// // In-Class tutorials -->
+
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-not-found",
+  templateUrl: "./not-found.component.html",
+  styleUrls: ["./not-found.component.css"],
+})
+export class NotFoundComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
